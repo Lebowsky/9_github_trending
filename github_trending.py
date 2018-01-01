847e436c7abc31277834efbff4d5d5fc2fa8b26b
Andrey Lebowski <andrey.lebowski@gmail.com>
2018-01-01 20:15:47 +0300

    code optimization and increased readability

--- github_trending.py
+++ github_trending.py
@@ -3,45 +3,40 @@ import datetime
 
 
 def get_trending_repositories(top_size):
-    day_of_week = 7
+    days_a_week = 7
     now = datetime.datetime.now()
-    date_week_earlier = now.date() - datetime.timedelta(days=day_of_week)
+    last_week = now.date() - datetime.timedelta(days=days_a_week)
 
-    query = requests.get(
+    response = requests.get(
         'https://api.github.com/search/repositories',
         params={
-            'q': 'created:>={}'.format(str(date_week_earlier)),
+            'q': 'created:>={}'.format(str(last_week)),
             'sort': 'stars',
         },
     )
-    repositories = query.json()['items'][:top_size]
+    repositories = response.json()['items'][:top_size]
     return repositories
 
 
 def get_open_issues_amount(repo_owner, repo_name):
-    query = requests.get(
-        'https://api.github.com/repos/{}/{}/issues'.format(
-            repo_owner, repo_name),
+    response = requests.get(
+        'https://api.github.com/repos/{}/{}/issues'.format(repo_owner, repo_name),
         params={'state': 'open'},
     )
-    return len(query.json())
+    return len(response.json())
 
 
 if __name__ == '__main__':
     top_size_repo = 20
     repos = get_trending_repositories(top_size_repo)
 
-    list_repos = []
-    for repo in repos:
+    for index, repo in enumerate(repos):
         repo_owner = repo['owner']['login']
         repo_name = repo['name']
         repo_url = repo['html_url']
         issues_amount = get_open_issues_amount(repo_owner, repo_name)
-        list_repos.append({'repo_url': repo_url, 'issues_data': issues_amount})
-
-    for repo in list_repos:
         print(
-            list_repos.index(repo) + 1,
-            repo['repo_url'],
-            repo['issues_data']
+            index + 1,
+            repo_url,
+            ' ({} open issues)'.format(issues_amount),
         )
